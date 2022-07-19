f8bd729d58a91a1722171c5ed1c613a03ed2034c
Kevin Presley <kevinpresley1995@gmail.com>
2022-07-19 12:58:59 -0300

    pegando dados dos input e salvando no array

--- script/script.js
+++ script/script.js
@@ -1,17 +1,48 @@
 
 class Produto {
     constructor() {
-        this.id = idGenerator();
-        this.name = nameProduto;
-        this.valor = 0;
+        this.id = '';
+        this.arrayProduto = [];
     }
 
     salvar(){
-        console.log(produto.name)
-        console.log(produto.id)
+        this.id = this.idGenerator();
+        let produto =  this.lerDados();
+        if(this.validaCampos(produto)){
+        this.addProduto(produto);
+       }
+       
+       console.log(this.arrayProduto)
     }
-
-    excluir(){
+    lerDados(){
+        let produto = {}
+        produto.id = this.id;
+        produto.nameProduto = document.getElementById('produto').value;
+        produto.valor = document.getElementById('preço').value;
+        return produto;
+    }
+    validaCampos(produto){
+        let msg='';
+        if(produto.nameProduto ==''){
+            msg += `Digite um produto!!`
+        }
+        if(produto.valor ==''){
+            msg += `Digite um preço!!`
+        }
+        if(msg != ''){
+            alert(msg)
+            return false;
+        }
+        return true;
+    }
+    addProduto(produto){
+        this.arrayProduto.push(produto);
+    }
+    idGenerator(){
+        let id= Math.floor(Math.random()*1000)
+        return id
+     }
+    cancelar(){
 
     }
 }
