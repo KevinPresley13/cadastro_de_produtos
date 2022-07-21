31a542bd10a98924d9f08bfac28ad151edaecd55
Kevin Presley <kevinpresley1995@gmail.com>
2022-07-21 12:16:07 -0300

    imprindo dados da tabela na tela

--- script/script.js
+++ script/script.js
@@ -11,7 +11,7 @@ class Produto {
         if(this.validaCampos(produto)){
         this.addProduto(produto);
        }
-       
+       this.updateTabela();
        console.log(this.arrayProduto)
     }
     lerDados(){
@@ -38,12 +38,32 @@ class Produto {
     addProduto(produto){
         this.arrayProduto.push(produto);
     }
+    updateTabela(){
+        let tbody = document.getElementById('tbody');
+        let tr = '<tr>'
+        let array = this.arrayProduto;
+        array.forEach(produto =>{
+            tr += `<td>${produto.id}</td>
+                   <td>${produto.nameProduto}</td>
+                   <td>${produto.valor}</td>
+                   <td>
+                        <img src="./images/edit.svg" alt="editar">
+                        <img src="./images/delet.svg" alt="excluir">
+                   </td>`;
+            tr += '</tr>'
+        })
+        
+        tbody.innerHTML = tr;
+        document.querySelector('#produto').value = '';
+        document.querySelector('#preço').value = '';
+    }
     idGenerator(){
         let id= Math.floor(Math.random()*1000)
         return id
      }
     cancelar(){
-
+        document.querySelector('#produto').value = '';
+        document.querySelector('#preço').value = '';
     }
 }
 function idGenerator(){
