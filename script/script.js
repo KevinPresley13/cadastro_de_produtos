38f3d04b941bd994dc24cc3fc79904a841e3e8c6
Kevin Presley <kevinpresley1995@gmail.com>
2022-07-25 12:39:14 -0300

    finalisada a funçao editar

--- script/script.js
+++ script/script.js
@@ -2,16 +2,21 @@
 class Produto {
     constructor() {
         this.arrayProduto = [];
+        this.editId = null;
     }
 
     salvar(){
         this.id = this.idGenerator();
         let produto =  this.lerDados();
         if(this.validaCampos(produto)){
-        this.addProduto(produto);
-       }
-       this.updateTabela();
-    }
+            if(this.editId == null){
+                this.addProduto(produto);
+            }else{
+                this.atualizar(this.editId,produto)
+            }
+        }
+        this.updateTabela();
+    }   
     lerDados(){
         let produto = {}
         produto.id = this.id;
@@ -45,15 +50,14 @@ class Produto {
             <td>${this.arrayProduto[i].nameProduto}</td>
             <td>${this.arrayProduto[i].valor}</td>
             <td>
-                 <img id-data=${this.arrayProduto[i].id} onclick="produto.edit("${JSON.stringify(this.arrayProduto[i])}") src="./images/edit.svg" alt="editar">
+                 <img id-data=${this.arrayProduto[i].id} onclick='produto.edit(${JSON.stringify(this.arrayProduto[i])})'src="./images/edit.svg" alt="editar">
                  <img id-data=${this.arrayProduto[i].id} onclick="produto.delete(this)" src="./images/delet.svg" alt="excluir">
             </td>`;
             tr += '</tr>'
         }
         
         tbody.innerHTML = tr;
-        document.querySelector('#produto').value = '';
-        document.querySelector('#preço').value = '';
+        this.cancelar();
     }
     idGenerator(){
         let id= Math.floor(Math.random()*1000)
@@ -62,9 +66,22 @@ class Produto {
     cancelar(){
         document.querySelector('#produto').value = '';
         document.querySelector('#preço').value = '';
+        document.querySelector('#salvar').innerText = 'Salvar';
+        this.editId = null;
     }
-    edit(array){
-        alert(array)
+    atualizar(id,produto){
+        for(let i = 0; i<this.arrayProduto.length; i++){
+            if(this.arrayProduto[i].id == id){
+                this.arrayProduto[i].nameProduto = produto.nameProduto;
+                this.arrayProduto[i].valor = produto.valor;
+            };
+        }
+    }
+    edit(dados){
+        this.editId = dados.id;
+        document.querySelector('#produto').value = dados.nameProduto;
+        document.querySelector('#preço').value = dados.valor;
+        document.querySelector('#salvar').innerText = 'Atualizar';
     }
     delete(id){
         this.arrayProduto = this.arrayProduto.filter(produto=>produto.id != id.getAttribute('id-data'))
